674ab03bcad225b85a23ebb8ea8a888c3a34a099
daemon1024 <barun1024@gmail.com>
2020-04-20 00:51:31 +0530

    feat: commit and pr to github on mutation

---
+++ src/ghop/index.js
@@ -0,0 +1,42 @@
+const GithubAPI = require("./gitcommitpr");
+const editMarkdown = require("./editmarkdown");
+
+async function ghop(meetupData, token) {
+  let readmecontent = await editMarkdown(meetupData);
+  console.log("Markdown fetched and edited");
+  let api = new GithubAPI({
+    token: token
+  });
+  api.setRepo("daemon1024", "meetups");
+  console.log("repo set to daemon1024/meetups");
+  api
+    .setBranch("meetup-api")
+    .then(() => {
+      console.log("pushing files");
+      api.pushFiles("Add new meetup on " + meetupData.date, [
+        { content: readmecontent, path: "README.md" }
+      ]);
+    })
+    .then(function() {
+      console.log("Files committed!");
+    })
+    .then(() => {
+      api
+        .createpr({
+          title: "added new meetup via api",
+          body: "Please pull these in!",
+          head: "meetup-api",
+          base: "master"
+        })
+        .then(() => console.log("made a pr"));
+    });
+}
+
+module.exports = ghop;
+
+// let meetupData = {
+//   date: "2020-01-01",
+//   topic: "Dk",
+//   details: "Doesn't matter",
+//   speaker: "someone on earth"
+// };
